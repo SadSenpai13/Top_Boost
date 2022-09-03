505202bb52b8216891228178e4c9d9ba6c7d2fb1
Roman Melnychuk <roman4iek02@gmail.com>
2022-09-03 16:33:25 +0300

    fonts incl

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TopBoost</title>
     <link rel="stylesheet" href="css/style.css">
+    
+<!-- FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" />
+    <link href="https://www.dafontfree.net/embed/cm9ndWUtaGVyby1jb25kZW5zZWQtY29uZGVuc2VkJmRhdGEvMTUvci83MjU5MS9yb2d1ZWhlcm9jb25kLnR0Zg" rel="stylesheet" type="text/css"/>
 </head>
 <body>
     <h1>test</h1>
