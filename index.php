4b9e72fc966e5579832a08bbc88ea0670089d3ec
Roman Melnychuk <roman4iek02@gmail.com>
2022-09-05 19:23:45 +0300

    test

--- index.php
+++ index.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TopBoost</title>
+    <title>Anonime</title>
     <link rel="stylesheet" href="css/style.css">
-    
+
 <!-- FONTS -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" />
-    <link href="https://www.dafontfree.net/embed/cm9ndWUtaGVyby1jb25kZW5zZWQtY29uZGVuc2VkJmRhdGEvMTUvci83MjU5MS9yb2d1ZWhlcm9jb25kLnR0Zg" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic" rel="stylesheet" />
 </head>
 <body>
-    <h1>test</h1>
+        
 
     <script src="js/script.js"></script>
 </body>
